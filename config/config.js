051c5ee0e0f3356e66f7c0aa52714aa25b3dec0e
cloud-cb <646932161@qq.com>
2017-05-09 18:18:17 +0800

    新增环境配置文件

---
+++ config/config.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const env = 'dev'; // 这里配置环境
+
+var baseURL = (function(env) {
+
+    var baseURL,
+        urls,
+    urls = {
+        dev: 'http://192.168.1.11:8080',
+        test: 'http://192.168.1.11:8080',
+        prod: ''
+    };
+    if (env === 'dev') {
+        baseURL = urls.dev;
+    }
+    if (env === 'test') {
+        baseURL = urls.test;
+    }
+    if (env === 'prod') {
+        baseURL = urls.prod;
+    }
+    return baseURL;
+
+})(env);
+
+export {
+    baseURL
+}
